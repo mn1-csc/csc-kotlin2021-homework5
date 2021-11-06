57bb20f77540e047f0f7628a29e25da54e97900e
mikhail.nasimov <mike.nasimov@compscicenter.ru>
2021-11-06 15:45:15 +0300

    initial

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    application
+}
+group = "ru.senin.kotlin.wiki"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.apurebase:arkenv:3.1.0")
+    implementation("org.apache.commons:commons-compress:1.20")
+    implementation(kotlin("reflect"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation(kotlin("test-junit5"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+kotlin.sourceSets.all {
+    languageSettings.apply {
+        optIn("kotlin.time.ExperimentalTime")
+    }
+}
